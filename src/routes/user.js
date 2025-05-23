9f54fc0ba74f8d01cd5fe03e8d7f6ee6807fe5ec
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-05-23 16:09:22 +0530

    Add routes and refactor code

---
+++ src/routes/user.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+// Models
+const User = require("../models/user");
+
+// ------------------------ Routes ------------------------ //
+
+const router = express.Router();
+
+// Get User Feed
+router.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.status(200).send({ message: "User feed fetched successfully", users });
+  } catch (error) {
+    res.status(400).send("ERROR: " + error?.message);
+  }
+});
+
+module.exports = router;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,8 +1,10 @@
 const validator = require("validator");
 
+// Validate user signup data
 const validateSignupData = (req) => {
   const { firstName, lastName, emailId, password } = req.body;
 
+  // Validate data
   if (!firstName || !lastName) {
     throw new Error("Name is not valid");
   } else if (!validator.isEmail(emailId)) {
@@ -12,9 +14,11 @@ const validateSignupData = (req) => {
   }
 };
 
+// Validate user login data
 const validateLoginData = (req) => {
   const { emailId, password } = req.body;
 
+  // Validate data
   if (!validator.isEmail(emailId)) {
     throw new Error("Email is not valid");
   } else if (!validator.isStrongPassword(password)) {
@@ -22,4 +26,51 @@ const validateLoginData = (req) => {
   }
 };
 
-module.exports = { validateSignupData, validateLoginData };
+// Validate user profile edit data
+const validateProfileEditData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+
+  // Check if data is valid
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  // Validate data
+  if (!isEditAllowed) {
+    throw new Error("Invalid edit request");
+  }
+};
+
+// Validate user password edit data
+const validatePasswordEditData = (req) => {
+  const { newPassword } = req.body;
+
+  // Check if data is valid
+  const allowedEditFields = ["oldPassword", "newPassword"];
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  // Validate data
+  if (!isEditAllowed) {
+    throw new Error("Invalid password update request");
+  } else if (!validator.isStrongPassword(newPassword)) {
+    throw new Error("New Password is not Strong");
+  }
+};
+
+module.exports = {
+  validateSignupData,
+  validateLoginData,
+  validateProfileEditData,
+  validatePasswordEditData,
+};
