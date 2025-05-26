d9870d859010822bf0f88a03ebda7d301c528a76
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-05-26 15:11:21 +0530

    Update config

--- src/app.js
+++ src/app.js
@@ -26,7 +26,8 @@ const app = express();
 const port = process.env.PORT;
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
